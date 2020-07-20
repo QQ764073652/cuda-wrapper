bc18ff2ed8a74283abc6a5d3928b8e8d329b6926
ruanxingbaozi <ruanxingbaozi@dingtalk.com>
2020-07-20 10:26:44 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,2 @@
-CUDA=9.2.148-1
-CUDA_SHORT=9.2
-CUDA_APT=9-2
-UBUNTU_VERSION=ubuntu1604
-
 mkdir -p release
 gcc -I /usr/local/cuda/include/ cuda-wrapper-prod-v1.2.c -fPIC -shared -ldl -lcuda -o ./release/libcuda.so
-
-find release -type f -name *so -exec mv '{}' '{}'.${CUDA_SHORT} \;
